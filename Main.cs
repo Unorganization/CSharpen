899a861c66ced03dd1aa8753feb22319d5f25485
dev <dev@dev.com>
2022-05-28 11:51:55 -0700

    .

--- Main.cs
+++ Main.cs
@@ -2,12 +2,6 @@ using System;
 
 public class Program {
   public static void Main(string[] args) {
-    // V1_Original.Run();
-    // V15.Run();
-    // V2.Run();
-    // V21.Run();
-    // V22.Run();
-    V23.Run();
-    // V3.Run();
+    Console.WriteLine("Hello");
   }
 }

b939295d458be4ff71cce03a0fcfc0e611591fa9
dev <dev@dev.com>
2022-05-28 12:41:13 -0700

    .

--- V020.cs
+++ V020.cs
@@ -20,25 +20,25 @@ public class V020
 
   static Func<TracingString, TracingString> TracingUpperCase = i => 
   {
-    var logger = i.logger.Log($"BEFORE UpperCase({i.str})");
+    var logger = i.logger.Log($"BEFORE: {i.str})");
     var str = UpperCase(i.str);
-    logger = logger.Log($"AFTER UpperCase({str})");
+    logger = logger.Log($"AFTER: {str}");
     return TracingString.Lift(str, logger);
   };
   
   static Func<TracingString, TracingString> TracingFirstWord = i => 
   {
-    var logger = i.logger.Log($"BEFORE FirstWord({i.str})");
+    var logger = i.logger.Log($"BEFORE: {i.str})");
     var str = FirstWord(i.str);
-    logger = logger.Log($"AFTER FirstWord({str})");
+    logger = logger.Log($"AFTER: {str}");
     return TracingString.Lift(str, logger);
   };
   
   static Func<TracingString, TracingString> TracingFixE = i => 
   {
-    var logger = i.logger.Log($"BEFORE FixE({i.str})");
+    var logger = i.logger.Log($"BEFORE: {i.str})");
     var str = FixE(i.str);
-    logger = logger.Log($"AFTER FixE({str})");
+    logger = logger.Log($"AFTER: {str}");
     return TracingString.Lift(str, logger);
   };
   

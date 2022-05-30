8a7aa37cf32c20401007eb611d3326524845eeb2
dev <dev@dev.com>
2022-05-30 01:56:23 -0700

    .

--- V033.cs
+++ V033.cs
@@ -12,57 +12,53 @@ public static class V033
     static Func<string, Writer> FirstWordWithLog = Writer.Lift(FirstWord, "Called FirstWord");
     static Func<string, Writer> FixEWithLog = Writer.Lift(FixE, "Called FixE");
 
-    interface IMonad<TValue, TPayload>
+    interface IMonad<TValue, TPayload, TDerived>
     {
         TValue Value { get; set; }
         TPayload Payload { get; set; }
 
-        TPayload GetInitialPayload(TValue value);
+        TPayload Initialize(TValue value);
 
-        TPayload AfterRun(TPayload oldPayload, TPayload newPayload, TValue value);
+        TPayload HandleRun(TPayload oldPayload, TPayload newPayload, TValue value);
+
+        TDerived Create(TValue value, TPayload payload);
     }
 
-    class Monad<TValue, TPayload, TDerived> where TDerived : IMonad<TValue, TPayload>, new()
+    class Monad<TValue, TPayload, TDerived> where TDerived : IMonad<TValue, TPayload, TDerived>, new()
     {
         public TValue Value { get; set; }
         public TPayload Payload { get; set; }
 
         public static Func<TValue, TDerived> Lift(Func<TValue, TValue> func, TPayload payload) => v => new TDerived { Value = func(v), Payload = payload };
-        // public static TDerived Create(TValue value) => new TDerived { Value = value, Payload = $"Initial value: {value}" };
+
         public static TDerived Create(TValue value)
         {
-            var w = new TDerived();
-            w.Value = value;
-            w.Payload = w.GetInitialPayload(value);
-            return w;
+            var result = new TDerived();
+            return result.Create(value, result.Initialize(value));
         }
 
-        public TDerived Run(Func<TValue, TDerived> func)
-        {
-            var w = func(Value);
+        public TDerived Run(Func<TValue, TDerived> func) => func(Value).Pipe(w => new TDerived().Create(w.Value, w.HandleRun(this.Payload, w.Payload, w.Value)));
 
-            var w2 = new TDerived();
-            w2.Value = w.Value;
-            w2.Payload = w.AfterRun(this.Payload, w.Payload, w.Value); ;
-            return w2;
+        public static Writer Run(string input, Func<string, Writer>[] list)
+        {
+            return list.Aggregate(
+                Writer.Create(input),
+                (accum, i) => accum.Run(i)
+            );
         }
     }
 
-    class Writer : Monad<string, string, Writer>, IMonad<string, string>
+    class Writer : Monad<string, string, Writer>, IMonad<string, string, Writer>
     {
-        public string GetInitialPayload(string value) => $"Initial value: {value}";
+        public string Initialize(string value) => $"Initial value: {value}";
+        public string HandleRun(string oldPayload, string newPayload, string value) => $"{oldPayload}\n{newPayload}: {value}";
 
-        public string AfterRun(string oldPayload, string newPayload, string value) => $"{oldPayload}\n{newPayload}: {value}";
-
-        // public static Writer Create(string value) => new Writer { Value = value, Payload = $"Initial value: {value}" };
-
-        // public static Func<string, Writer> Lift(Func<string, string> func, string log) => v => new Writer { Value = func(v), Payload = log };
-
-        // public Writer Run(Func<string, Writer> func)
-        // {
-        //     var neww = func(this.Value);
-        //     return new Writer { Value = neww.Value, Payload = $"{this.Payload}\n{neww.Payload}: {neww.Value}" };
-        // }
+        public Writer Create(string value, string payload)
+        {
+            Value = value;
+            Payload = payload;
+            return this;
+        }
     }
 
     public static void Run()
